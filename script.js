b1eb91aa31b9e6da1abfa91438090825f569db92
Thaís Moreira <thaisdmedeiros@gmail.com>
2025-02-24 19:07:37 +0100

    update html and js

---
+++ script.js
@@ -0,0 +1,64 @@
+let todo = document.getElementById("todo");
+let addTodo = document.getElementById("addTodo");
+let deleteTodo = document.getElementById("deleteTodo");
+
+let totalTodo = []
+
+
+// function todoList(todo) {
+//    if (addTodo) === true
+//       return todo
+// }
+
+// function calcula() {
+//    var resultado = n1.valueAsNumber + n2.valueAsNumber;
+
+//    if (isNaN(resultado)) {
+//     r.textContent = '';
+//    }else
+//    r.textContent = resultado;
+//  }
+
+// document.addEventListener('input', function() {
+//    var resultado = n1.valueAsNumber + n2.valueAsNumber;
+//    r.textContent = isNaN(resultado) ? "" : resultado;
+//  });
+
+
+
+// var fslist = document.querySelectorAll(".multiple-field");
+
+// for (var i = 0; i < fslist.length; i++) {
+//   initMultipleFieldSet(fslist[i]);
+// }
+
+// function initMultipleFieldSet(fs) {
+//   var addButton = document.createElement("button");
+//   addButton.textContent = "Adicionar";
+//   addButton.type = "button";
+
+//   fs.appendChild(addButton);
+
+//   var firstInput = fs.querySelector("input");
+
+//   addButton.addEventListener("click", function () {
+//     var div = document.createElement("div");
+//     var newInput = document.createElement("input");
+//     newInput.name = firstInput.name;
+//     newInput.type = firstInput.type;
+
+//     var deleteButton = document.createElement("button");
+//     deleteButton.textContent = "Excluir";
+//     deleteButton.type = "button";
+
+//     div.appendChild(newInput);
+//     div.appendChild(deleteButton);
+
+//     deleteButton.addEventListener("click", function() {
+//       div.remove(); //maneira mais fácil
+//       //fs.removeChild(div); pode ser usado esse codigo também
+//     });
+
+//     fs.insertBefore(div, addButton);
+//   });
+// }
